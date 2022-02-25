1396c6c0073c95f99d357a6da8c04a0a3815f93f
Silas White <silas.j.white@gmail.com>
2022-02-24 19:52:40 -0500

    Error checking complete with testing

--- expression.cc
+++ expression.cc
@@ -17,65 +17,92 @@ using std::endl;
  */
 
 string Expression::convertToPostfix(string infix) {
-  if (infix.size() == 0) {
-    throw SyntaxError(0, "Operand expected");
-  }
-
+  
+  // Error checking variables
   bool expectOperator = false;
   int parenCount = 0;
+  stack<int> parenPos;
+
+  // Variables for the algorithm
   stack<char> operators;
   string postfix = "";
+
   for (int i = 0; i < infix.length(); i++) {
     if (infix[i] > 47 && infix[i] < 58) {
-      // Error checking
+
+      // Error checking: make sure operand expected
       if (expectOperator) {
         throw SyntaxError(i, "Operator expected");
       }
       expectOperator = true;
 
+
       postfix += infix[i];
+
     } else if (infix[i] == '(') {
-      operators.push(infix[i]);
+
+      // Error checking: make sure operator before open paren
+      if (expectOperator) {
+        throw SyntaxError(i, "Operator expected");
+      }
+
+      // Error checking: Keep track of opened paren position and value
+      parenPos.push(i);
       parenCount++;
+
+
+      operators.push(infix[i]);
+
     } else if (infix[i] == ')') {
+
+      // Error Checking: Make sure paren was expected
       if (parenCount == 0) {
         throw SyntaxError(i, "Unbalanced parentheses");
       }
+      // Error checking: make sure closing paren is after an operand
+      if (!expectOperator) {
+        throw SyntaxError(i, "Operand expected");
+      }
+      parenPos.pop();
+      parenCount--;
+
+
       while (operators.top() != '(') {
         postfix += operators.top();
         operators.pop();
       }
       // Remove the '('
       operators.pop();
-      parenCount--;
+
     } else if (infix[i] == '*' || 
                 infix[i] == '+' || 
                 infix[i] == '-' || 
                 infix[i] == '/') {
-      // Error Checking
+      // Error checking: make sure operator expected
       if (!expectOperator) {
         throw SyntaxError(i, "Operand expected");
       }
       expectOperator = false;
+
 
       while (!operators.empty() && precedence(infix[i]) <= precedence(operators.top())) {
         postfix += operators.top();
         operators.pop();
       }
       operators.push(infix[i]);
+
     } else {
-      // Error checking
-      throw SyntaxError(i, "Invalid Character");
+      // If not a valid mathematical operation, throw error
+      throw SyntaxError(i, "Invalid character");
     }
   }
 
-  if (parenCount != 0) {
-    for (int i = 0; i < infix.length(); i++) {
-      if (infix[i] == '(') {
-        int unbalancedParen = i;
-        throw SyntaxError(unbalancedParen, "Unbalanced parentheses");
-      }
-    }
+  if (parenCount > 0) {
+    throw SyntaxError(parenPos.top(), "Unbalanced parentheses");
+  }
+
+  if (!expectOperator) {
+    throw SyntaxError(infix.size(), "Operand expected");
   }
 
   while (!operators.empty()) {
@@ -128,7 +155,7 @@ string Expression::convertToPrefix(string postfix) {
 int precedence(char c) {
   if (c == '/' || c == '*') {
     return 2;
-  } else if (c == '+' || '-') {
+  } else if (c == '+' || c == '-') {
     return 1;
   } else {
     return 0;
--- expression_test.cc
+++ expression_test.cc
@@ -1,6 +1,7 @@
 #include "gtest/gtest.h"
 #include "expression.h"
 
+
 TEST(postfix, two) {
   Expression e("2");
   EXPECT_EQ("2", e.getPostfix());
@@ -32,9 +33,14 @@ TEST(postfix, parenAddMultParenDiv) {
   EXPECT_EQ("13+24+*", e.getPostfix());
 }
 
-TEST(postfix, someParens) {
-  Expression e("(1+3)*(2+4)");
-  EXPECT_EQ("13+24+*", e.getPostfix());
+TEST(postfix, longExpression) {
+  Expression e("3*((2+4)*(1+1)+7)/(2*5+9)");
+  EXPECT_EQ("324+11+*7+*25*9+/", e.getPostfix());
+}
+
+TEST(postfix, longerExpression) {
+  Expression e("7+4*(2+4*(7-2)*(5*(4-2))/(5*4*5))");
+  EXPECT_EQ("742472-*542-**54*5*/+*+", e.getPostfix());
 }
 
 TEST(postfix, multipleParentheses) {
@@ -42,17 +48,17 @@ TEST(postfix, multipleParentheses) {
   EXPECT_EQ("2", e.getPostfix());
 }
 
-TEST(postfix, hw23aPostfix) {
+TEST(postfix, hw23a) {
   Expression e("(7+2)*(3+4)-1");
   EXPECT_EQ("72+34+*1-", e.getPostfix());
 }
 
-TEST(postfix, hw23bPostfix) {
+TEST(postfix, hw23b) {
   Expression e("8-((4+1)*2-5)/9");
   EXPECT_EQ("841+2*5-9/-", e.getPostfix());
 }
 
-TEST(postfix, hw23cPostfix) {
+TEST(postfix, hw23c) {
   Expression e("(3*2+1)/(4+5)");
   EXPECT_EQ("32*1+45+/", e.getPostfix());
 }
@@ -62,11 +68,50 @@ TEST(evaluate, two) {
   EXPECT_EQ(2, e.getValue());
 }
 
-TEST(evaluate, someParens) {
+TEST(evaluate, someAddition) {
+  Expression e("2+3+4");
+  EXPECT_EQ(9, e.getValue());
+}
+
+TEST(evaluate, addMult) {
+  Expression e("2+3+4+3*4");
+  EXPECT_EQ(21, e.getValue());
+}
+
+TEST(evaluate, addMultDiv) {
+  Expression e("2+3*4+5*6/2");
+  EXPECT_EQ(29, e.getValue());
+}
+
+TEST(evaluate, twoParen) {
+  Expression e("(2)");
+  EXPECT_EQ(2, e.getValue());
+}
+
+TEST(evaluate, parenAddMultParenDiv) {
   Expression e("(1+3)*(2+4)");
   EXPECT_EQ(24, e.getValue());
 }
 
+TEST(evaluate, longExpression) {
+  Expression e("3*((2+4)*(1+1)+7)/(2*5+9)");
+  EXPECT_EQ(3, e.getValue());
+}
+
+TEST(evaluate, longerExpression) {
+  Expression e("7+4*(2+4*(7-2)*(5*(4-2))/(5*4*5))");
+  EXPECT_EQ(23, e.getValue());
+}
+
+TEST(evaluate, multipleParentheses) {
+  Expression e("(((2)))");
+  EXPECT_EQ(2, e.getValue());
+}
+
+TEST(evaluate, hw23a) {
+  Expression e("(7+2)*(3+4)-1");
+  EXPECT_EQ(62, e.getValue());
+}
 
 // The tests below illustrate the error messages
 // (exception  descriptions) my tests will look for.
@@ -96,7 +141,7 @@ TEST(postfixError, plus) {
   }
 }
 
-TEST(postfixError, missingOp) {
+TEST(postfixError, missingOperator) {
   try {
     Expression e("1+23");
     e.getPostfix();
@@ -107,6 +152,106 @@ TEST(postfixError, missingOp) {
   }
 }
 
+TEST(postfixError, missingOperator2) {
+  try {
+    Expression e("0+12*3");
+    e.getPostfix();
+    FAIL() << "Didn't throw the expected exception.";
+  } catch(SyntaxError &e) {
+    EXPECT_EQ(3, e.getPosition());
+    EXPECT_EQ("Operator expected", e.getDescription());
+  }
+}
+
+TEST(postfixError, missingOperand) {
+  try {
+    Expression e("0+*1+2");
+    e.getPostfix();
+    FAIL() << "Didn't throw the expected exception.";
+  } catch(SyntaxError &e) {
+    EXPECT_EQ(2, e.getPosition());
+    EXPECT_EQ("Operand expected", e.getDescription());
+  }
+}
+
+TEST(postfixError, missingOperand2) {
+  try {
+    Expression e("*1+2");
+    e.getPostfix();
+    FAIL() << "Didn't throw the expected exception.";
+  } catch(SyntaxError &e) {
+    EXPECT_EQ(0, e.getPosition());
+    EXPECT_EQ("Operand expected", e.getDescription());
+  }
+}
+
+TEST(postfixError, missingOperand3) {
+  try {
+    Expression e("1+2+");
+    e.getPostfix();
+    FAIL() << "Didn't throw the expected exception.";
+  } catch(SyntaxError &e) {
+    EXPECT_EQ(4, e.getPosition());
+    EXPECT_EQ("Operand expected", e.getDescription());
+  }
+}
+
+TEST(postfixError, missingOperator3) {
+  try {
+    Expression e("1+2(3+4");
+    e.getPostfix();
+    FAIL() << "Didn't throw the expected exception.";
+  } catch(SyntaxError &e) {
+    EXPECT_EQ(3, e.getPosition());
+    EXPECT_EQ("Operator expected", e.getDescription());
+  }
+}
+
+TEST(postfixError, missingOperator4) {
+  try {
+    Expression e("(1+2)3+4");
+    e.getPostfix();
+    FAIL() << "Didn't throw the expected exception.";
+  } catch(SyntaxError &e) {
+    EXPECT_EQ(5, e.getPosition());
+    EXPECT_EQ("Operator expected", e.getDescription());
+  }
+}
+
+TEST(postfixError, missingOperand4) {
+  try {
+    Expression e("(*4)+(2*4)");
+    e.getPostfix();
+    FAIL() << "Didn't throw the expected exception.";
+  } catch(SyntaxError &e) {
+    EXPECT_EQ(1, e.getPosition());
+    EXPECT_EQ("Operand expected", e.getDescription());
+  }
+}
+
+TEST(postfixError, missingOperand5) {
+  try {
+    Expression e("(2*)+(2*4)");
+    e.getPostfix();
+    FAIL() << "Didn't throw the expected exception.";
+  } catch(SyntaxError &e) {
+    EXPECT_EQ(3, e.getPosition());
+    EXPECT_EQ("Operand expected", e.getDescription());
+  }
+}
+
+TEST(postfixError, missingOperand6) {
+  try {
+    Expression e("((1*3)+)");
+    e.getPostfix();
+    FAIL() << "Didn't throw the expected exception.";
+  } catch(SyntaxError &e) {
+    EXPECT_EQ(7, e.getPosition());
+    EXPECT_EQ("Operand expected", e.getDescription());
+  }
+}
+
+
 TEST(postfixError, missingParen) {
   try {
     Expression e("1)");
@@ -115,5 +260,113 @@ TEST(postfixError, missingParen) {
   } catch(SyntaxError &e) {
     EXPECT_EQ(1, e.getPosition());
     EXPECT_EQ("Unbalanced parentheses", e.getDescription());
+  }
+}
+
+TEST(postfixError, missingParen2) {
+  try {
+    Expression e("(1*3)+(2*4");
+    e.getPostfix();
+    FAIL() << "Didn't throw the expected exception.";
+  } catch(SyntaxError &e) {
+    EXPECT_EQ(6, e.getPosition());
+    EXPECT_EQ("Unbalanced parentheses", e.getDescription());
+  }
+}
+
+TEST(postfixError, missingParen3) {
+  try {
+    Expression e("((((2)))");
+    e.getPostfix();
+    FAIL() << "Didn't throw the expected exception.";
+  } catch(SyntaxError &e) {
+    EXPECT_EQ(0, e.getPosition());
+    EXPECT_EQ("Unbalanced parentheses", e.getDescription());
+  }
+}
+
+TEST(postfixError, missingParen4) {
+  try {
+    Expression e("1+2)");
+    e.getPostfix();
+    FAIL() << "Didn't throw the expected exception.";
+  } catch(SyntaxError &e) {
+    EXPECT_EQ(3, e.getPosition());
+    EXPECT_EQ("Unbalanced parentheses", e.getDescription());
+  }
+}
+
+TEST(postfixError, missingParen5) {
+  try {
+    Expression e("(1*3))");
+    e.getPostfix();
+    FAIL() << "Didn't throw the expected exception.";
+  } catch(SyntaxError &e) {
+    EXPECT_EQ(5, e.getPosition());
+    EXPECT_EQ("Unbalanced parentheses", e.getDescription());
+  }
+}
+
+TEST(postfixError, missingParen6) {
+  try {
+    Expression e("(1*3))(");
+    e.getPostfix();
+    FAIL() << "Didn't throw the expected exception.";
+  } catch(SyntaxError &e) {
+    EXPECT_EQ(5, e.getPosition());
+    EXPECT_EQ("Unbalanced parentheses", e.getDescription());
+  }
+}
+
+TEST(postfixError, missingParen7) {
+  try {
+    Expression e("(1*3))+(2");
+    e.getPostfix();
+    FAIL() << "Didn't throw the expected exception.";
+  } catch(SyntaxError &e) {
+    EXPECT_EQ(5, e.getPosition());
+    EXPECT_EQ("Unbalanced parentheses", e.getDescription());
+  }
+}
+
+TEST(evaluateError, divByZero) {
+  try {
+    Expression e("3/0");
+    e.getValue();
+    FAIL() << "Didn't throw the expected exception.";
+  } catch(DivideByZeroError &e) {
+    EXPECT_EQ(2, e.getPosition());
+  }
+}
+
+TEST(evaluateError, divByZero2) {
+  try {
+    Expression e("3/(1-1)");
+    e.getValue();
+    FAIL() << "Didn't throw the expected exception.";
+  } catch(DivideByZeroError &e) {
+    EXPECT_EQ(4, e.getPosition());
+  }
+}
+
+TEST(postfixError, invalidCharacter) {
+  try {
+    Expression e("a+2");
+    e.getValue();
+    FAIL() << "Didn't throw the expected exception.";
+  } catch(SyntaxError &e) {
+    EXPECT_EQ(0, e.getPosition());
+    EXPECT_EQ("Invalid character", e.getDescription());
+  }
+}
+
+TEST(postfixError, invalidCharacter2) {
+  try {
+    Expression e("1a2");
+    e.getValue();
+    FAIL() << "Didn't throw the expected exception.";
+  } catch(SyntaxError &e) {
+    EXPECT_EQ(1, e.getPosition());
+    EXPECT_EQ("Invalid character", e.getDescription());
   }
 }
